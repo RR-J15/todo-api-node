8ce02fd4ffe131d3820416732e9282dae6689482
Johanes SOSSOU <johanessossou65@gmail.com>
2025-08-14 12:26:18 +0100

    nouvelle modification

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,9 @@
- const User = require ("../ models / User ");
+ const User = require ("../ models/User");
  const jwt = require ("jsonwebtoken");
  const bcrypt = require ("bcrypt");
 
  exports.register = async (req , res ) => {
-
  const { name, email , password } = req .body;
-
 try{
  const user = await User. create ({name, email , password });
  res . status (201). json( { message : " Utilisateur creé" ,user });
@@ -14,7 +12,7 @@ try{
  }
 };
 
-exports.login = async (req , res )=>{
+exports.login = async(req , res)=>{
        const { email , password } = req .body;
        try {
        const user = await User. findOne({ email });
