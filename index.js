897946b24bcb5929594f87b7d528d910e84e41c0
RR-J15 <johanessossou65@gmail.com>
2025-08-14 11:49:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose ");
+const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const todoRoutes = require("./ routes / todoRoutes ");
-const authRoutes = require("./ routes / authRoutes ");
+const todoRoutes = require("./routes/todoRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(cors());
